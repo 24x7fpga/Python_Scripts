0c51b9681904553091e7ad53daf01a77b8fda64b
24x7fpga <kiran.24x7fpga@gmail.com>
2024-08-29 12:01:01 -0400

    added uvm run script

---
+++ uvm_scripts/uvm_run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+name = sys.argv[1]
+home = os.environ['HOME']
+
+t_path = home + "/Projects/fpgaProjects/UVM/uvm_tcl_script/uvm_run.tcl"
+d_path = home + "/Projects/fpgaProjects/UVM/uvm_verification/"+name
+
+
+vivado_command = [
+    "vivado",
+    "-mode", "batch",
+    "-source", t_path,
+    "-tclargs", name, d_path
+]
+
+subprocess.run(vivado_command)
+
+
+
+
