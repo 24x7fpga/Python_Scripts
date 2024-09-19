11bf2dc31942d8f4919ea1156547fd951926e10f
24x7fpga <kiran.24x7fpga@gmail.com>
2024-09-18 21:48:35 -0400

    updated uvm_clean script

--- uvm_scripts/uvm_clean.py
+++ uvm_scripts/uvm_clean.py
@@ -8,8 +8,11 @@ home = os.environ['HOME']
 
 path = home + "/Projects/fpgaProjects/UVM/uvm_verification/*/verif"
 
+v_path = home+ "/vivado*"
+
 try:
     os.system("rm -rf "+path)
+    os.system("rm -f "+v_path)
     print("Cleaned! ;)")
 except:
     print("could not execute command")
