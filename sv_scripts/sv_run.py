0de24c37a9d14a36a9472ccdfde12013d97771bc
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-04 16:08:51 -0400

    python script to execute the testbench design

---
+++ sv_scripts/sv_run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+name = sys.argv[1]
+home = os.environ['HOME']
+
+t_path = home + "/Projects/fpgaProjects/Verification/sv_tcl_script/sv_run.tcl"
+d_path = home + "/Projects/fpgaProjects/Verification/sv_verification/"+name
+
+
+vivado_command = [
+    "vivado",
+    "-mode", "batch",
+    "-source", t_path,
+    "-tclargs", name, d_path
+]
+
+subprocess.run(vivado_command)
