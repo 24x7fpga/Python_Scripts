5c1c982ba0c3991272e911f895210228782bfc1b
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-07 13:54:50 -0400

    added verification scripts

---
+++ sv_scripts/sv_clean.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+home = os.environ['HOME']
+
+path = home + "/Projects/fpgaProjects/SystemVerilog_Verification/sv_verification/*/verif"
+
+v_path = home+ "/vivado*"
+
+try:
+    os.system("rm -rf "+path)
+    os.system("rm -f "+v_path)
+    print("Cleaned! ;)")
+except:
+    print("could not execute command")
--- sv_scripts/sv_design.py
+++ sv_scripts/sv_design.py
@@ -6,7 +6,7 @@ import sys
 name = sys.argv[1]
 home = os.environ['HOME']
 
-d_path  = home + "/Projects/fpgaProjects/Verification/sv_verification"
+d_path  = home + "/Projects/fpgaProjects/SystemVerilog_Verification/sv_verification"
 
 # create design and test-bench folders
 try:
@@ -38,7 +38,7 @@ try:
 
     # locate the design file
     loc = ["// Local Variables: \n",
-           "// verilog-library-directories:(\"~/Projects/fpgaProjects/Verification/sv_verification/"+name+"/*\") \n",
+           "// verilog-library-directories:(\"~/Projects/fpgaProjects/SystemVerilog_Verification/sv_verification/"+name+"/*\") \n",
 	   "// End:"]
     print('Emac linkers ;)')
 
