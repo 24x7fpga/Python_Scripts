3f0d4b063121ec8f502f7e508cd0a6c2fb2005d0
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-28 22:27:58 -0400

    checks if the folder is already created

--- iVerilog/iv_design.py
+++ iVerilog/iv_design.py
@@ -9,152 +9,156 @@ home = os.environ['HOME']
 d_path  = home + "/Projects/FPGA_Projects/iVerilog/design"
 tb_path = home + "/Projects/FPGA_Projects/iVerilog/tb_design"
 
+#check if the folder already exists
+if name in os.listdir(d_path):
+    	print('----------- WARNING -----------')
+    	print('   folder name already exists   ')
+else:
+	# create design and test-bench folders
+	try:
+	    os.chdir(d_path)
+	    os.mkdir(name)
+	    os.chdir(tb_path)
+	    os.mkdir("tb_"+name)
+	    print('Directory creation successful ;)')
+	except:
+	    print('Directory creation Failed ;(')
 
-# create design and test-bench folders
-try:
-    os.chdir(d_path)
-    os.mkdir(name)
-    os.chdir(tb_path)
-    os.mkdir("tb_"+name)
-    print('Directory creation successful ;)')
-except:
-    print('Directory creation Failed ;(')
+	# create design files
+	try:
+	    nw_d=d_path+"/"+name+"/"
+	    os.chdir(nw_d)
+	    f_d=open(name+".sv","x")
+	    print('Design File Created ;)')
+	   
+	    # timescale
+	    scale = ["`timescale 1ns/1ns \n"] 
+	    print('Timescale ;)')
 
-# create design files
-try:
-    nw_d=d_path+"/"+name+"/"
-    os.chdir(nw_d)
-    f_d=open(name+".sv","x")
-    print('Design File Created ;)')
-   
-    # timescale
-    scale = ["`timescale 1ns/1ns \n"] 
-    print('Timescale ;)')
+	    # write Design module, clock and resets
+	    module = ["module "+name+ "(/*AUTOARG*/); \n",
+		      "  input logic clk; \n",
+		      "  input logic rst; \n"]
+	    print('Module Written ;)')
 
-    # write Design module, clock and resets
-    module = ["module "+name+ "(/*AUTOARG*/); \n",
-	      "  input logic clk; \n",
-	      "  input logic rst; \n"]
-    print('Module Written ;)')
+	    # auto wire and register 
+	    rc = ["  /*AUTOREG*/ \n",
+		  "  /*AUTOWIRE*/ \n"]
+	    print('Auto WIRE and REG ;)')
 
-    # auto wire and register 
-    rc = ["  /*AUTOREG*/ \n",
-	  "  /*AUTOWIRE*/ \n"]
-    print('Auto WIRE and REG ;)')
+	    # DUT instantiation
+	    init = ["<module> MOD1 (/*AUTOINST*/); \n"]
+	    print('Module Instantiation ;)')
 
-    # DUT instantiation
-    init = ["<module> MOD1 (/*AUTOINST*/); \n"]
-    print('Module Instantiation ;)')
+	    # endmodule
+	    end = ["endmodule \n"]
+	    print('EndModule ;)')
 
-    # endmodule
-    end = ["endmodule \n"]
-    print('EndModule ;)')
+	    # locate the design file
+	    loc = ["// Local Variables: \n",
+		   "// verilog-library-directories:(\"~/Projects/FPGA_Projects/iVerilog/design/"+name+"/\") \n",
+		   "// End:"]
+	    print('Emac linkers ;)')
 
-    # locate the design file
-    loc = ["// Local Variables: \n",
-           "// verilog-library-directories:(\"~/Projects/FPGA_Projects/iVerilog/design/"+name+"/\") \n",
-	   "// End:"]
-    print('Emac linkers ;)')
+	    # add a lines
+	    line = [" \n"]
 
-    # add a lines
-    line = [" \n"]
+	    # write all the lines to a single list
+	    content = scale + module + line + rc + line + init + line + end + loc
+	    print('Files content Stitched ;)')
 
-    # write all the lines to a single list
-    content = scale + module + line + rc + line + init + line + end + loc
-    print('Files content Stitched ;)')
+	    # write to the file
+	    f_d.writelines(content)
+	    print('Design file written ;)')
 
-    # write to the file
-    f_d.writelines(content)
-    print('Design file written ;)')
+	    # close the file
+	    f_d.close()
+	    print('File Closed ;)')
+	except:
+	    print('Design File Creation Failed ;( ')
+	    
+	 # create test-bench files
+	try:
+	    nw_tb=tb_path+"/tb_"+name+"/"
+	    os.chdir(nw_tb)
+	    f_tb=open("tb_"+name+".sv","x")
+	    print('Test-Bench File Created ;)')
 
-    # close the file
-    f_d.close()
-    print('File Closed ;)')
-except:
-    print('Design File Creation Failed ;( ')
-    
- # create test-bench files
-try:
-    nw_tb=tb_path+"/tb_"+name+"/"
-    os.chdir(nw_tb)
-    f_tb=open("tb_"+name+".sv","x")
-    print('Test-Bench File Created ;)')
+	    # timescale
+	    scale = ["`timescale 1ns/1ns \n"] 
+	    print('Timescale ;)')
 
-    # timescale
-    scale = ["`timescale 1ns/1ns \n"] 
-    print('Timescale ;)')
+	    # write test-bench module
+	    tb_module = ["module tb_"+name+ "(); \n"]
+	    print('Module Written ;)')
 
-    # write test-bench module
-    tb_module = ["module tb_"+name+ "(); \n"]
-    print('Module Written ;)')
-
-    # clock and reset
-    tb_rc = ["localparam t = 10; \n",
-             "logic clk; \n",
-	     "logic rst; \n"]
-    print('Clocks and Resets ;)')
-
-
-    # write verilog AUTO 
-    tb_auto = ["/*AUTOREG*/ \n",
-               "/*AUTOWIRE*/ \n"]
-    print('Verilog AUTO mode ;)')
+	    # clock and reset
+	    tb_rc = ["localparam t = 10; \n",
+		     "logic clk; \n",
+		     "logic rst; \n"]
+	    print('Clocks and Resets ;)')
 
 
-    # DUT instantiation
-    tb_init = [name+ " DUT (/*AUTOINST*/); \n"]
-    print('Design Module Instantiated ;)')
+	    # write verilog AUTO 
+	    tb_auto = ["/*AUTOREG*/ \n",
+		       "/*AUTOWIRE*/ \n"]
+	    print('Verilog AUTO mode ;)')
 
-    # initiate clock
-    tb_clk = ["always #(t/2) clk = (clk === 1'b0);\n"]
-    print('Clocks Initialized ;)')
 
-    # initiate reset
-    tb_rst = ["initial begin \n",
-              "rst = 0; \n",
-              "#(2*t); \n",
-              "rst = 1; \n",
-              "end \n"]
-    print('Reset Initialized')
+	    # DUT instantiation
+	    tb_init = [name+ " DUT (/*AUTOINST*/); \n"]
+	    print('Design Module Instantiated ;)')
 
-    # dump file
-    tb_dump = ["initial begin \n",
-	       "$dumpfile(\"tb_"+name+".vcd\"); \n",
-	       "$dumpvars(0,tb_"+name+"); \n",
-	       "end \n"]
-    print('.vcd Dump ;)')
+	    # initiate clock
+	    tb_clk = ["always #(t/2) clk = (clk === 1'b0);\n"]
+	    print('Clocks Initialized ;)')
 
-    # endmodule
-    tb_end = ["endmodule \n"]
-    print('EndModule ;)')
+	    # initiate reset
+	    tb_rst = ["initial begin \n",
+		      "rst = 0; \n",
+		      "#(2*t); \n",
+		      "rst = 1; \n",
+		      "end \n"]
+	    print('Reset Initialized')
 
-    # locate the design file
-    tb_loc = ["// Local Variables: \n",
-              "// verilog-library-directories:(\"~/Projects/FPGA_Projects/iVerilog/design/"+name+"/\" \".\") \n",
-	      "// End:"]
-    print('Emac linkers ;)')
+	    # dump file
+	    tb_dump = ["initial begin \n",
+		       "$dumpfile(\"tb_"+name+".vcd\"); \n",
+		       "$dumpvars(0,tb_"+name+"); \n",
+		       "end \n"]
+	    print('.vcd Dump ;)')
 
-    # write all the lines to a single list
-    tb_content = scale + tb_module + tb_rc + tb_auto + tb_init + tb_clk + tb_rst + tb_dump + tb_end + tb_loc
-    print('Files content Stitched ;)')
+	    # endmodule
+	    tb_end = ["endmodule \n"]
+	    print('EndModule ;)')
 
-    # write to the file
-    f_tb.writelines(tb_content)
-    print('Test-bench file written ;)')
+	    # locate the design file
+	    tb_loc = ["// Local Variables: \n",
+		      "// verilog-library-directories:(\"~/Projects/FPGA_Projects/iVerilog/design/"+name+"/\" \".\") \n",
+		      "// End:"]
+	    print('Emac linkers ;)')
 
-    # close the file
-    f_tb.close()
-    print('File Closed ;)')
-except:
-    print('Test-Bench File Creation Failed ;( ')
-    
+	    # write all the lines to a single list
+	    tb_content = scale + tb_module + tb_rc + tb_auto + tb_init + tb_clk + tb_rst + tb_dump + tb_end + tb_loc
+	    print('Files content Stitched ;)')
 
-# open emacs
-#try:
-#    os.system("emacs tb_"+name+".v")
-#    print("Test-bench successful ;)")
-#except:
-#    print("Test-bench failure ;(")
+	    # write to the file
+	    f_tb.writelines(tb_content)
+	    print('Test-bench file written ;)')
+
+	    # close the file
+	    f_tb.close()
+	    print('File Closed ;)')
+	except:
+	    print('Test-Bench File Creation Failed ;( ')
+	    
+
+	# open emacs
+	#try:
+	#    os.system("emacs tb_"+name+".v")
+	#    print("Test-bench successful ;)")
+	#except:
+	#    print("Test-bench failure ;(")
 
 
 
