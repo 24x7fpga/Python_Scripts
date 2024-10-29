2d92906b161445a93c972c656fc1c028663a2a88
24x7fpga <kiran.24x7fpga@gmail.com>
2024-10-29 18:19:54 -0400

    updated run file to automatically choose all .sv and .svh files for simulation

--- iVerilog/iv_run.py
+++ iVerilog/iv_run.py
@@ -4,6 +4,7 @@ import os
 import sys
 import glob 
 
+
 name = sys.argv[1]
 home = os.environ['HOME']
 
@@ -13,17 +14,17 @@ length = len(sys.argv[1:])
 path = home + "/Projects/FPGA_Projects/iVerilog/design/"+name+"/" 
 
 # check file extension
-if(glob.glob(path+name+".v")):
-    print("Verilog File")
-    ext = ".v"
-else:
-    print("SystemVerilog File")
-    ext = ".sv"
+#if(glob.glob(path+name+".v")):
+#    print("Verilog File")
+#    ext = ".v"
+#else:
+#    print("SystemVerilog File")
+#    ext = ".sv"
 
-print(ext)
+#print(ext)
 #top-module with data path
-d_path  = home + "/Projects/FPGA_Projects/iVerilog/design/"+name+"/"+name+ext
-
+#d_path  = home + "/Projects/FPGA_Projects/iVerilog/design/"+name+"/"+name+ext
+d_path  = home + "/Projects/FPGA_Projects/iVerilog/design/"+name+"/"
 # test bench path
 tb_path = home + "/Projects/FPGA_Projects/iVerilog/tb_design/tb_"+name+"/"
 
@@ -39,14 +40,33 @@ if(length > 1):
             d_path = d_path +" "+subd_path
 
 
+
+
+
+all_d_files = os.listdir(path)
+d_files = [file for file in all_d_files if ".sv" in file or ".svh" in file]
+#print(d_files)
+
+for i in range(len(d_files)):
+    subd_path = path + d_files[i] 
+    d_path = d_path +" "+subd_path  
+
+all_tb_files = os.listdir(tb_path)
+tb_files = [file for file in all_tb_files if ".sv" in file or ".svh" in file]
+tb_files = " ".join(tb_files)
+#print(tb_files)
+
+#############################
+# os.system("ls -l")
+
+# move to testbench directory
 os.chdir(tb_path)
 print(tb_path)
 print(os.getcwd())
 
-# os.system("ls -l")
-
 try:
-    os.system("iverilog -g2012 -o tb_"+name+".vvp tb_"+name+ext+" "+d_path)
+    os.system("iverilog -g2012 -o tb_"+name+".vvp "+tb_files+" "+d_path)
+    print("iverilog -g2012 -o tb_"+name+".vvp "+tb_path+tb_files+" "+d_path)
     print("iverilog command successful ;)")
     os.system("ls -l")
 except:
