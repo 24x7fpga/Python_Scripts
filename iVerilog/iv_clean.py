798a1aff005b26990276d11de26ac27dbbb67325
24x7fpga <kiran.24x7fpga@gmail.com>
2024-11-06 16:17:12 -0500

    updated to remove *.vvp and *.vcd files from the path

--- iVerilog/iv_clean.py
+++ iVerilog/iv_clean.py
@@ -10,8 +10,8 @@ path = home + "/Projects/FPGA_Projects/iVerilog"
 
 
 try:
-    os.system("rm -rf "+path+"/*/*/*.vvp")
-    os.system("rm -rf "+path+"/*/*/*.vcd")
+    os.system("find "+path+" -type f \( -name \"*.vvp\" -o -name \"*.vcd\" \) -print")
+    os.system("find "+path+" -type f \( -name \"*.vvp\" -o -name \"*.vcd\" \) -exec rm -r {} +")
     print("Cleaned! ;)")
 except:
     print("could not execute command")
