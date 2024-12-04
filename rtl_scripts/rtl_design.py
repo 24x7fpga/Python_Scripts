22a45587b4081ee12b8968cd31a48f76936c3164
24x7fpga <kiran.24x7fpga@gmail.com>
2024-12-04 15:23:55 -0500

    script for rtl design automation

---
+++ rtl_scripts/rtl_design.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+name = sys.argv[1]
+home = os.environ['HOME']
+
+proj_path  = home + "/Projects/FPGA_Projects/RTL/rtl_designs"
+temp_path  = home + "/Projects/FPGA_Projects/RTL/rtl_templates"
+
+# create a folder is the project name
+try:
+    os.chdir(proj_path)
+    os.mkdir(name)
+    print('Directory creation successful ;)')
+except:
+    print('Directory creation Failed ;(')
+
+# copy the template file into the source directory
+try:    
+    os.system("cp -f "+temp_path+"/* "+proj_path+"/"+name)
+    print('Copied the Template Files ;)')
+except:
+    print('Copying Templates Failed ;(')
+    
+# rename design and top module 
+try:
+    os.chdir(proj_path+"/"+name)  
+    os.system("mv design.sv "+name+".sv")  
+    os.system("mv tb_design.sv tb_"+name+".sv")
+    print('Design Rename Successful ;)')
+except:
+    print('Design Rename Failed ;(')
+
+# rename the design files
+try:
+    os.chdir(proj_path+"/"+name)  
+    os.system("sed -i 's/$ARG/"+name+"/g' "+name+".sv")
+    os.system("sed -i 's/$ARG/"+name+"/g' tb_"+name+".sv")  
+    os.system("sed -i 's/$ARG/"+name+"/g' interface.sv")
+    print('Renamed the Designed Files  ;)')
+except:
+    print('Renaming Failed ;(')
