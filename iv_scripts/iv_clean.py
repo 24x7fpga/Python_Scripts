6d633fd60af4552b99393d07b9fd8a5899af3427
24x7fpga <kiran.24x7fpga@gmail.com>
2024-12-05 17:21:44 -0500

    updated iverilog run script

--- iv_scripts/iv_clean.py
+++ iv_scripts/iv_clean.py
@@ -8,7 +8,6 @@ home = os.environ['HOME']
 
 path = home + "/Projects/FPGA_Projects/RTL"
 
-
 try:
     os.system("find "+path+" -type f \( -name \"*.vvp\" -o -name \"*.vcd\" \) -print")
     os.system("find "+path+" -type f \( -name \"*.vvp\" -o -name \"*.vcd\" \) -exec rm -r {} +")
