d3e870b7fb3fcf9978eb453ffe0d38997d7ca290
24x7fpga <kiran.24x7fpga@gmail.com>
2024-12-06 12:43:16 -0500

    updated vivado scripts to check if design exists

--- vivado_scripts/vivado_run.py
+++ vivado_scripts/vivado_run.py
@@ -10,6 +10,17 @@ home = os.environ['HOME']
 t_path = home + "/Projects/FPGA_Projects/RTL/rtl_tcl_scripts/rtl_run.tcl"
 d_path = home + "/Projects/FPGA_Projects/RTL/rtl_designs/"+name
 
+# Check if the proj exists and overwrite
+if os.path.isdir(d_path+"/proj"):
+    print('----------- Project Overwite -----------')
+    subprocess.run(["rm", "-rf", d_path+"/proj"], check=True)
+
+
+# Create the proj folder 
+os.mkdir(d_path+"/proj")
+
+# Invoke Vivado from inside the proj folder 
+os.chdir(d_path+"/proj")
 
 vivado_command = [
     "vivado",
