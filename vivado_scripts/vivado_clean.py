24216bcf170d426a9169f2b2a3e6157417fec36d
24x7fpga <kiran.24x7fpga@gmail.com>
2024-12-10 19:05:18 -0500

    updated vivado clean script

--- vivado_scripts/vivado_clean.py
+++ vivado_scripts/vivado_clean.py
@@ -8,14 +8,10 @@ home   = os.environ['HOME']
 
 path   = home + "/Projects/FPGA_Projects/RTL/rtl_designs/"
 
-v_log  = home + "/Projects/FPGA_Projects/RTL/"
-
 v_path = home+ "/vivado*"
 
 try:
     os.system("find "+path+" -type d -name \"proj\" -exec rm -r {} +")
-    os.system("rm -d "+v_log+".Xil")
-    os.system("rm -f "+v_log+"vivado*")
     os.system("rm -f "+v_path)
     print("Cleaned! ;)")
 except:
