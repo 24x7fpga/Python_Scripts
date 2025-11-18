a2c4a4f7aab3e4b50c2cfd1b18917defbe2b69ec
24x7fpga <kiran.24x7fpga@gmail.com>
2025-11-17 22:15:19 -0500

    updated clean script

--- clean_scripts/clean_all.py
+++ clean_scripts/clean_all.py
@@ -14,11 +14,11 @@ v_log    = home + "/Projects/FPGA_Projects/RTL/rtl_designs/"
 
 try:
     # iVerilog
-    os.system("find "+rtl_path+" -type f \( -name \"*.vvp\" -o -name \"*.vcd\" \) -print")
-    os.system("find "+rtl_path+" -type f \( -name \"*.vvp\" -o -name \"*.vcd\" \) -exec rm -r {} +")
+    os.system("find "+rtl_path+" -type f \\( -name \"*.vvp\" -o -name \"*.vcd\" \\) -print")
+    os.system("find "+rtl_path+" -type f \\( -name \"*.vvp\" -o -name \"*.vcd\" \\) -exec rm -r {} +")
     # Verilator
-    os.system("find "+rtl_path+" -type d \( -name \"obj_dir\" \) -print")
-    os.system("find "+rtl_path+" -type d \( -name \"obj_dir\" \) -exec rm -r {} +")
+    os.system("find "+rtl_path+" -type d \\( -name \"obj_dir\" \\) -print")
+    os.system("find "+rtl_path+" -type d \\( -name \"obj_dir\" \\) -exec rm -r {} +")
     # SV
     os.system("find "+sv_path+" -type d -name \"verif\" -print")
     os.system("find "+sv_path+" -type d -name \"verif\" -exec rm -r {} +")
